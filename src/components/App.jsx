001a334d5b95791b87eff02a137ee6fc1ceeb307
joepherdegz <142654533+joepherdegz@users.noreply.github.com>
2024-06-12 19:03:22 -0700

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,36 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+import { Component } from 'react';
+export class App extends Component {
+  state = {
+    good: 1,
+    neutral: 0,
+    bad: 0
+  };
+   
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad; 
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    return (
+    <>
+      <div>
+          <p>
+            Good: <span>{good}</span>
+          </p>
+          <p>
+            Neutral: <span>{neutral}</span>
+          </p>
+          <p>
+            Bad: <span>{bad}</span>
+          </p>
+          <p>
+            Total: <span>{total}</span>
+          </p>
+      </div>
+    </>
   );
 };
+}
